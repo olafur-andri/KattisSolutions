085786deff285f0bcc5badc45128fed9bdd761ce
Olafur Andri <olafur1199@gmail.com>
2021-12-29 23:54:00 +0000

    added clarifying comment to explain how I solved it

--- 03_3dprinter/main.py
+++ 03_3dprinter/main.py
@@ -1,5 +1,9 @@
 import math
 
+# fastest way to print `n` statues is simply to double the printers every day
+# until you've reached or surpassed `n`. Then you simply need one day to print
+# the remaining statues.
+
 def solve(n: int):
   if n == 0: return 0
   return math.ceil(math.log2(n)) + 1
